7bd816402edcc0c1b896a874494cc51ad6113744
Mauricio Molina <tica.molina57@gmail.com>
2025-03-24 17:25:03 -0300

    Copio desde App al Contexto el useState de usuarios y usuariosAEditar, el useEffect para la ejecución de la función getAll, esta función, la de agregarUsuairo, borrarUsuario y editarUsuario. En el objeto data la información a compartir va a ser, usuarios, usuariosAEditar, setUsuarioAEditar, agregarUsuario, borrarUsuario y editarUsuario

--- src/contexts/UsersContext.jsx
+++ src/contexts/UsersContext.jsx
@@ -5,10 +5,107 @@ const UsersContext = createContext()
 
 const UsersProvider = ( {children }) => {
 
-    const [users, setUsers] = useState([])
+  const [usuarios, setUsuarios] = useState(null)
+  const [usuarioAEditar, setUsuarioAEditar] = useState(null)
+
+  useEffect(() => {
+    getAllUsuarios()
+  }, [])
+  
+  const getAllUsuarios = async () => {
+
+    try {
+      const res = await fetch(import.meta.env.VITE_BACKEND)
+
+      if (!res.ok) {
+        throw new Error ('No pudo hacerse la petición')
+      }
+      const data = await res.json()
+      setUsuarios(data);
+    } catch (error) {
+      console.error(error.message)
+    }
+  }
+
+  const agregarUsuario = async (nuevoUsuario) => {
+    delete nuevoUsuario.id
+
+    try {
+      const res = await fetch(import.meta.env.VITE_BACKEND, {
+        method: 'POST',
+        headers: {'content-type': 'application/json'},
+        body: JSON.stringify(nuevoUsuario)
+      })
+
+      if (!res.ok) {
+        throw new Error ('No se pudo hacer la petición')
+      }
+
+      const usuarioAgregadoEnBackend = await res.json()
+      const nuevoEstadoUsuarios = [...usuarios, usuarioAgregadoEnBackend]
+      setUsuarios(nuevoEstadoUsuarios)
+    } catch (error) {
+      console.error(error.message)
+    }
+
+
+  }
+
+  const borrarUsuario = async (id) => {
+
+    const urlBorrado = import.meta.env.VITE_BACKEND + id
+
+    try {
+      const res = await fetch(urlBorrado, {
+        method: 'DELETE'
+      })
+
+      if (!res.ok) {
+        throw new Error ('No se pudo hacer la petición')
+      }
+
+      const usuarioEliminadoDelBackend = await res.json()
+
+    } catch (error) {
+      console.error(error.message);
+    }
+
+    const nuevoEstadoUsuarios = usuarios.filter( user => user.id !== id)
+    setUsuarios(nuevoEstadoUsuarios)
+  }
+
+  const editarUsuario = async (usuarioEditado) => {
+
+    const urlEditar = import.meta.env.VITE_BACKEND + usuarioEditado.id
+
+    try {
+      usuarioEditado.edad = Number(usuarioEditado.edad)
+      const res = await fetch(urlEditar, {
+        method: 'PUT',
+        headers: {'content-type': 'application/json'},
+        body: JSON.stringify(usuarioEditado) 
+      })
+
+      if (!res.ok) {
+        throw new Error ('No se pudo hacer la petición')
+      }
+
+      const usuarioEditadoBackend = await res.json()
+      const nuevoEstadoUsuarios = usuarios.map(user => user.id === usuarioEditado.id ? usuarioEditado : user)
+      setUsuarios(nuevoEstadoUsuarios)
+
+    } catch (error) {
+      console.error(error.message)
+    }
+  }
 
     const data = {
-        users
+        usuarios,
+        usuarioAEditar, 
+        setUsuarioAEditar,
+        agregarUsuario,
+        borrarUsuario,
+        editarUsuario,
     }
 
     return <UsersContext.Provider value={data}> {children}</UsersContext.Provider>
